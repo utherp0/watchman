ef0de59752c84f600a58aa455cdb4517b43fd056
utherp0 <ian.lawson@redhat.com>
2021-05-26 13:35:30 +0100

    Added payload output

--- src/main/java/org/uth/watchman/Watchman.java
+++ src/main/java/org/uth/watchman/Watchman.java
@@ -4,19 +4,16 @@ import io.quarkus.funqy.*;
 import io.cloudevents.*;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.json.Json;
-import java.util.logging.*;
 
 public class Watchman 
 {
-  private static final Logger log = Logger.getLogger(Watchman.class.getName());
-
   @Funq
   public void process( byte[] input, @Context CloudEvent inputEvent )
   {
     ResponsePayload output = new ResponsePayload();
 
-    log.log(Level.INFO, "Called via event " + inputEvent.getType());
-
+    String payload = new String(input);
+    System.out.println( payload );
     //return output;
   }
 }

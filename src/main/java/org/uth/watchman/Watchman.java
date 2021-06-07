422e86ffa33a67f88b918bfb3ca836e73671a0e8
utherp0 <ian.lawson@redhat.com>
2021-06-07 12:37:14 +0100

    First drop of response code

--- src/main/java/org/uth/watchman/Watchman.java
+++ src/main/java/org/uth/watchman/Watchman.java
@@ -3,7 +3,10 @@ package org.uth.watchman;
 import io.quarkus.funqy.*;
 import io.quarkus.funqy.knative.events.*;
 import io.vertx.core.json.JsonObject;
-import io.vertx.core.json.Json;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.uth.watchman.utils.Postman;
 
@@ -21,10 +24,27 @@ public class Watchman
     System.out.println( payload );
     System.out.println( inputEvent );
 
+    // Parse the input
+    JsonObject message = new JsonObject(payload);
+
+    JsonObject messageFrom = message.getJsonObject("from");
+    String sender = messageFrom.getString("first_name") + " " + messageFrom.getString("last_name");
+    String messageText = message.getString("text");
+    JsonObject messageChat = message.getJsonObject("chat");
+    String chatID = messageChat.getString("id");
+
     String telegramTarget = "https://api.telegram.org/bot" + _bottoken + "/sendMessage";
 
     // Post a telegram response for the hell of it
     Postman postman = new Postman( telegramTarget );
+
+    Map<String,String> payloadOutput = new HashMap<>();
+
+    payloadOutput.put("chat_id", chatID );
+    payloadOutput.put("text", sender + " sent a message of length " + messageText.length());
+
+    postman.deliver(payloadOutput);
+
     //return output;
   }
 }

f33c21918f83c26996fd5146710a693a35afa0a8
utherp0 <ian.lawson@redhat.com>
2021-06-07 15:41:25 +0100

    Fix

--- src/main/java/org/uth/watchman/Processor.java
+++ src/main/java/org/uth/watchman/Processor.java
@@ -9,11 +9,6 @@ public class Processor
       return null;
     }
 
-    if( sender.indexOf("No9") != -1 )
-    {
-      return sender + " sent " + message.length() + " pointless rubbish as per usual";
-    }
-
     return sender + " sent " + message.length() + " characters...";
   }  
 }

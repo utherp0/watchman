40430ea95e8a46b1a410f392d903e2cca6f65855
utherp0 <ian.lawson@redhat.com>
2021-07-09 09:56:42 +0100

    Null message check

--- src/main/java/org/uth/watchman/Processor.java
+++ src/main/java/org/uth/watchman/Processor.java
@@ -4,6 +4,9 @@ public class Processor
 {
   public static String process( String sender, String message )
   {
+    // Check for null message
+    if( message == null ) return sender + " sent no text...";
+
     if( message.toLowerCase().indexOf("uthkamel") == -1 )
     {
       return null;
